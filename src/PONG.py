4bb75e02d762c32679897915ee9fdf379e3d0fc6
JakeEyes912 <stephjac004@gmail.com>
2026-05-11 10:04:40 -0500

    AI startup

--- src/PONG.py
+++ src/PONG.py
@@ -55,10 +55,15 @@ def handle_paddle_movement(keys, left_paddle, right_paddle, ball):
     if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
         left_paddle.move(up=False)
 
-    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
-        right_paddle.move(up=True)
-    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
-        right_paddle.move(up=False)
+def handle_ai_movement(ball, right_paddle):
+     if ball.y > right_paddle.y + right_paddle.height / 2:
+        if right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+            right_paddle.move(up=False)
+    
+    # If the ball is above the center of the paddle, move up
+        elif ball.y < right_paddle.y + right_paddle.height / 2:
+         if right_paddle.y - right_paddle.VEL >= 0:
+            right_paddle.move(up=True)
 
 class Ball:
     MAX_VEL = 5
@@ -71,7 +76,7 @@ class Ball:
         self.x_vel = self.MAX_VEL
         self.y_vel = 0
 
-    def draw(self, win):
+    def draw(self, win): 
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.radius)
 
     def move(self):
@@ -144,7 +149,8 @@ def main():
                 break
 
         keys = pygame.key.get_pressed()
-        handle_paddle_movement(keys, left_paddle, right_paddle, ball)
+        handle_paddle_movement(keys, left_paddle) # Only handles player (Left)
+        handle_ai_movement(right_paddle, ball)    # Automatically handles AI (Right)
 
         ball.move()
         handle_collision(ball, left_paddle, right_paddle)
